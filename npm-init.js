6eac3f2a5e4ab12e3a31bda44c855b5fa98ce127
Rafael Rinaldi <rafael@rinaldi.io>
2016-03-26 02:03:47 -0300

    feat(npm): fancier manifest template

--- npm-init.js
+++ npm-init.js
@@ -1,12 +1,34 @@
 /**
- * - Prompts for module name
+ * - Prompts for module name (default to current folder name)
  * - Prompts for module description
  * - Start module at v1.0.0
  * - Default license is MIT
+ * - If `npmrc` is available, fill the "author" key
  */
-module.exports = {
-  name: prompt('Module name', '', name => name),
+
+const run = require('child_process').execSync;
+const path = require('path');
+const home = path.resolve(process.env.HOME);
+const cwd = process.cwd().split('/').pop();
+const hasCredentials = require('fs').existsSync(`${home}/.npmrc`);
+
+var manifest = {
+  name: prompt('Module name', cwd, name => name),
   description: prompt('Description', '', description => description),
   version: '1.0.0',
   license: 'MIT'
 };
+
+if (hasCredentials) {
+  const npmrc = run(`cat ${home}/.npmrc`).toString();
+
+  manifest = Object.assign({
+    author: {
+      name: /name=(.*)$/m.exec(npmrc)[1],
+      email: /email=(.*)$/m.exec(npmrc)[1],
+      url: /url=(.*)$/m.exec(npmrc)[1]
+    }
+  }, manifest);
+}
+
+module.exports = manifest;
