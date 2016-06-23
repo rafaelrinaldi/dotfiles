40e8b409c78fe608172de900226c737819e07dff
Rafael Rinaldi <rafael@rinaldi.io>
2016-06-23 19:19:50 -0400

    feat(fish): remove nvim alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,13 @@
-set -Ux PATH /usr/local/bin $PATH
-set -Ux LSCOLORS ExFxCxDxBxegedabagacad
-set -Ux LC_CTYPE en_US.UTF-8
-set -Ux EDITOR vim
-
+set -g PATH /usr/local/bin $PATH
+set -g LSCOLORS ExFxCxDxBxegedabagacad
+set -g LC_CTYPE en_US.UTF-8
+set -g EDITOR vim
 set -g TERM xterm-256color-italic
+
+set -x HOMEBREW_GITHUB_API_TOKEN '99a1f94e55ae4bc4b7bc40bb8c74b4418dc1c6f2'
 
 set fish_color_search_match --background=white
 
 alias cask 'brew cask'
 alias git hub
 alias ip 'curl icanhazip.com'
-alias vim nvim
