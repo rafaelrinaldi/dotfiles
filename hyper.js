fec1d2abcfdbfaebb677c200a0c06b5a8ff6e7fa
Rafael Rinaldi <rafael@rinaldi.io>
2017-01-11 10:16:36 -0500

    feat(hyper): add hyper settings

---
+++ hyper.js
@@ -0,0 +1,19 @@
+module.exports = {
+  config: {
+    fontSize: 16,
+    fontFamily: '"SF Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    cursorShape: 'BLOCK',
+    padding: '10px',
+    bell: false,
+  },
+
+  plugins: [
+    'hyper-blink',
+    'hypercwd',
+    'hyper-papercolor',
+    'hyper-terminal-tabs'
+  ],
+
+  localPlugins: [
+  ]
+};
