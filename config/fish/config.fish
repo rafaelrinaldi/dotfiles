1715376c9f05352f9f85afcfbe589cb1aebd27ab
Rafael Rinaldi <rafaelrinaldi@users.noreply.github.com>
2017-03-11 14:04:36 -0500

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,8 +4,6 @@ set -g LC_CTYPE en_US.UTF-8
 set -g EDITOR vim
 set -g TERM xterm-256color-italic
 
-set -x HOMEBREW_GITHUB_API_TOKEN '99a1f94e55ae4bc4b7bc40bb8c74b4418dc1c6f2'
-
 set fish_color_search_match --background=white
 
 alias cask 'brew cask'
